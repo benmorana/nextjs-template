24c5bbb914a5ea82dbf189467ab4db941a021d44
Benoit Morana <benoit.morana@cancer.org.au>
2020-10-12 10:23:19 +1100

    Changing rewrite order in next config

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/:path*',
-        destination: `https://curly-waterfall-c6ef.ccsite.workers.dev/:path*?vercelURL=${process.env.VERCEL_URL}`,
-      },
-      {
         source: '/cloudflare-proxy/:path*',
         destination: '/cloudflare-proxy/:path*',
+      },
+      {
+        source: '/:path*',
+        destination: `https://curly-waterfall-c6ef.ccsite.workers.dev/:path*?vercelURL=${process.env.VERCEL_URL}`,
       }
     ]
   },
