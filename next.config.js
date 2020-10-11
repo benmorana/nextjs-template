77c8e1d16cac9e95ddc8c283c0bd352140193000
Benoit Morana <benoit.morana@cancer.org.au>
2020-10-12 10:32:25 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   async rewrites() {
     return [
+//       {
+//         source: '/cloudflare-proxy/:path*',
+//         destination: '/cloudflare-proxy/:path*',
+//       },
       {
-        source: '/cloudflare-proxy/:path*',
-        destination: '/cloudflare-proxy/:path*',
-      },
-      {
-        source: '/:path*',
+        source: '/:path*(^(?!cloudflare-proxy).*)',
         destination: `https://curly-waterfall-c6ef.ccsite.workers.dev/:path*?vercelURL=${process.env.VERCEL_URL}`,
       }
     ]
