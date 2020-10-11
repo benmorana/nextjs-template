c5e14a7fce5598ce9c10f78dca28f0174e66874d
Benoit Morana <benoit.morana@cancer.org.au>
2020-10-12 09:55:13 +1100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `https://curly-waterfall-c6ef.ccsite.workers.dev/:path*?vercelURL=${process.env.VERCEL_URL}`,
+      },
+      {
+        source: '/cloudflare-worker/:path*',
+        destination: '/:path*',
+      }
+    ]
+  },
+}
