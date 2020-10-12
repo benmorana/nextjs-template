37c108af7356501f2abffb9b3e2cf6d05511e881
Benoit Morana <benoit.morana@cancer.org.au>
2020-10-12 15:17:58 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ module.exports = {
           destination: `https://curly-waterfall-c6ef.ccsite.workers.dev/:path*?vercelURL=${process.env.VERCEL_URL}&buildID=${process.env.BUILD_ID}_stag&env=stag`,
         },
         {
+          source: '/',
+          destination: `https://curly-waterfall-c6ef.ccsite.workers.dev/?vercelURL=${process.env.VERCEL_URL}&buildID=${process.env.BUILD_ID}&env=${process.env.ENV}`,
+        },
+        {
           source: '/:path*',
           destination: `https://curly-waterfall-c6ef.ccsite.workers.dev/:path*?vercelURL=${process.env.VERCEL_URL}&buildID=${process.env.BUILD_ID}&env=${process.env.ENV}`,
         }
