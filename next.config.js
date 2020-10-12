d2cab7572fabd4cbf9fc04c80718aeeeeb95eafd
Benoit Morana <benoit.morana@cancer.org.au>
2020-10-12 17:47:55 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ module.exports = {
           destination: '/cloudflare-proxy/:path*',
         },
         {
-          source: '/:path*(\/?)',
-          destination: `https://curly-waterfall-c6ef.ccsite.workers.dev:path*vercelURL=${process.env.VERCEL_URL}&buildID=${process.env.BUILD_ID}&env=${process.env.ENV}`,
+          source: '/:path',
+          destination: `https://curly-waterfall-c6ef.ccsite.workers.dev/:path?vercelURL=${process.env.VERCEL_URL}&buildID=${process.env.BUILD_ID}&env=${process.env.ENV}`,
         }
       ];
     }
