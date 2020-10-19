0124bc2b1fa72ef84cb66be0a2fb7f05f14825cd
Benoit Morana <benoit.morana@cancer.org.au>
2020-10-19 12:19:38 +1100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,9 @@ class MyDocument extends Document {
   
   static async getInitialProps(ctx) {
     if(ctx.req && ctx.req.headers['Authorization'] !== 'Basic c3RhZ2luZzpzdGFnaW5n') {
-      res.statusCode = 401
-      res.setHeader('WWW-Authenticate', `Basic realm="Access private Cancer Council site"`)
-      res.end('401 Access Denied')
+      ctx.res.statusCode = 401
+      ctx.res.setHeader('WWW-Authenticate', `Basic realm="Access private Cancer Council site"`)
+      ctx.res.end('401 Access Denied')
     }
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
