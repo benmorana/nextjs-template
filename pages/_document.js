262ff70d868494c2265aacc6037650143489c0d8
Benoit Morana <benoit.morana@cancer.org.au>
2020-10-19 13:20:57 +1100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,6 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   
   static async getInitialProps(ctx) {
-    if(ctx.req && ctx.res && ctx.res.end && ctx.req.headers['Authorization'] !== 'Basic c3RhZ2luZzpzdGFnaW5n') {
-      console.log('Triggered auth');
-      ctx.res.statusCode = 401
-      ctx.res.setHeader('WWW-Authenticate', `Basic realm="Access private Cancer Council site"`)
-      ctx.res.end('Access Denied')
-    }
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
