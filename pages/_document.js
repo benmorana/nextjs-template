d0872f9ac72e2c5a0415173ea09f70093b22dc98
Benoit Morana <benoit.morana@cancer.org.au>
2020-10-19 12:20:15 +1100

    Adding logging

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ class MyDocument extends Document {
   
   static async getInitialProps(ctx) {
     if(ctx.req && ctx.req.headers['Authorization'] !== 'Basic c3RhZ2luZzpzdGFnaW5n') {
+      console.log('Triggered auth');
       ctx.res.statusCode = 401
       ctx.res.setHeader('WWW-Authenticate', `Basic realm="Access private Cancer Council site"`)
       ctx.res.end('401 Access Denied')
