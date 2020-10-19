9a05adeb28d723808116a3a17222af21d709808a
Benoit Morana <benoit.morana@cancer.org.au>
2020-10-19 12:17:28 +1100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,9 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  const authorization = 'Basic c3RhZ2luZzpzdGFnaW5n';
   
   static async getInitialProps(ctx) {
-    if(ctx.req && ctx.req.headers['Authorization'] !== authorization) {
+    if(ctx.req && ctx.req.headers['Authorization'] !== 'Basic c3RhZ2luZzpzdGFnaW5n') {
       res.statusCode = 401
       res.setHeader('WWW-Authenticate', `Basic realm="Access private Cancer Council site"`)
       res.end('401 Access Denied')
